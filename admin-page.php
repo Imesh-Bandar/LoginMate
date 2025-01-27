5cd654c7569470a54d0588a4da7efd19671004f9
Agathisi-Imesh-Bandar <124579120+Imesh-Bandar@users.noreply.github.com>
2025-01-27 16:15:45 +0530

    Design admin and user page

---
+++ admin-page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADMIN PAGE</title>
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="./css/style.css" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="content">
+            <h1>Hi, <span>Admin</span></h1>
+            <h2>Welcome <span>Admin</span></h2>
+            <p>Welcome to your admin page</p>
+            <a href="./login-form.php" class="btn">LOGIN</a>
+            <a href="./register-form.php" class="btn">REGISTER</a>
+            <a href="./logout.php" class="btn">LOGOUT</a>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
