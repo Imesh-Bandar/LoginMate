cc774375b99a83558a91e41b1d7d3b8e96a5fbca
Agathisi-Imesh-Bandar <124579120+Imesh-Bandar@users.noreply.github.com>
2025-01-27 16:49:15 +0530

    DESIGN USER LOGIN AND REGISTRATION

---
+++ login-form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN FORM</title>
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="./css/style.css" />
+</head>
+
+<body>
+    <div class="form-container">
+        <form action="" method="post">
+            <h3>LOGIN FORM</h3>
+            <input type="email" name="email" id="email" required placeholder="Enter your Emmail" />
+            <input type="passowrd" name="password" id="password" required placeholder="Enter your Password" />
+            <input type="submit" name="submit" value="register now" class="form-btn" />
+            <p>Don't Have an account <a href="./register-form.php">REGISTER-NOW</a> </p>
+        </form>
+    </div>
+</body>
+
+</html>
